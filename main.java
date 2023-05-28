af8ee0943a72ac1ae895ffcc652dcc3587774875
nitainedvetzki <106998867+nitainedvetzki@users.noreply.github.com>
2023-05-28 17:32:47 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,8 @@
+import com.google.common.collect.ImmutableList;  // Import classes from the Guava library
+
+public class Main {
+    public static void main(String[] args) {
+        ImmutableList<String> list = ImmutableList.of("Hello", "World");
+        System.out.println(list);  // Output: [Hello, World]
+    }
+}
