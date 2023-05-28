56660854c63b18666e93e2b2d73f560206bac7cc
nitainedvetzki <106998867+nitainedvetzki@users.noreply.github.com>
2023-05-28 17:36:37 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,8 +1,17 @@
-import com.google.common.collect.ImmutableList;  // Import classes from the Guava library
+import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    
     public static void main(String[] args) {
         ImmutableList<String> list = ImmutableList.of("Hello", "World");
-        System.out.println(list);  // Output: [Hello, World]
+        LOGGER.info("List: {}", list);
+        
+        String str = "   Example String   ";
+        String trimmed = StringUtils.trim(str);
+        LOGGER.info("Trimmed String: '{}'", trimmed);
     }
 }
